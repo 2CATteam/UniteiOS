a699fe8722a61fa5be97edc83ac5795b87b1f5da
Daniel Royer <DanielRoyer@daniels-macbook-pro.local>
2018-08-10 18:50:24 -0700

    Initial Commit

---
+++ Unite Client/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Unite Client
+//
+//  Created by Daniel Royer on 8/10/18.
+//  Copyright © 2018 Union Interpretation Team (UnITe). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
