58427527290cef340c02a6f2ffc293a074ef8986
Daniel Royer <DanielRoyer@daniels-macbook-pro.local>
2018-09-15 16:31:23 -0700

    Switched from StackView to CollectionView

---
+++ Unite Client/CollectionViewCell.swift
@@ -0,0 +1,81 @@
+//
+//  CollectionViewCell.swift
+//  Unite Client
+//
+//  Created by Daniel Royer on 9/15/18.
+//  Copyright © 2018 Union Interpretation Team (UnITe). All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    var button: UIButton?
+    
+    func displayContent(toDoName: String, isDone: Bool, instance: ViewController)
+    {
+        if (button == nil)
+        {
+            button = newButton(toDo: toDoName, isDone: isDone, instance: instance)
+            UIView.animate(withDuration: 1, animations: {
+                self.addSubview(self.button!)
+            })
+            let xConstraint = NSLayoutConstraint(item: button!, attribute: NSLayoutAttribute.centerX, relatedBy: NSLayoutRelation.equal, toItem: self, attribute: NSLayoutAttribute.centerX, multiplier: 1, constant: 0)
+            self.addConstraint(xConstraint)
+            let widthConstraint = NSLayoutConstraint(item: button!, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: UIScreen.main.bounds.size.width)
+            self.addConstraint(widthConstraint)
+            let heightConstraint = NSLayoutConstraint(item: button!, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: 50)
+            button!.addConstraint(heightConstraint)
+        }
+        button!.setTitle(toDoName, for: .normal)
+        if (isDone)
+        {
+            UIView.animate(withDuration: 1, animations: {
+                self.button!.layer.backgroundColor = UIColor(red: 0, green: 0.75, blue: 0, alpha: 1).cgColor
+            })
+        }
+        else
+        {
+            UIView.animate(withDuration: 1, animations: {
+                self.button!.layer.backgroundColor = UIColor.red.cgColor
+            })
+        }
+    }
+    
+    func newButton(toDo: String, isDone: Bool, instance: ViewController) -> UIButton
+    {
+        //Make a new button to return
+        let toReturn = UIButton(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+        
+        //Set button action
+        toReturn.addTarget(instance, action: #selector(instance.toggleButton), for: .touchUpInside)
+        
+        //Create and add a swipe gesture recognizer
+        let swiper = UISwipeGestureRecognizer(target: instance, action: #selector(instance.doSwipe))
+        swiper.delegate = instance
+        swiper.direction = .left
+        toReturn.addGestureRecognizer(swiper)
+        
+        //Set the button title and color
+        toReturn.setTitle(toDo, for: .normal)
+        if (isDone)
+        {
+            toReturn.layer.backgroundColor = UIColor.green.cgColor
+        }
+        else {
+            toReturn.layer.backgroundColor = UIColor.red.cgColor
+        }
+        
+        //Make the button look good
+        toReturn.layer.cornerRadius = 10
+        toReturn.layer.borderWidth = 2
+        toReturn.layer.borderColor = UIColor.white.cgColor
+        toReturn.layer.shadowColor = UIColor.black.cgColor
+        toReturn.layer.shadowOpacity = 0.2
+        toReturn.layer.shadowRadius = 2
+        toReturn.layer.shadowOffset = CGSize(width: 2, height: 2)
+        toReturn.translatesAutoresizingMaskIntoConstraints = false
+        
+        //Return
+        return toReturn
+    }
+}
--- Unite Client/ViewController.swift
+++ Unite Client/ViewController.swift
@@ -9,18 +9,45 @@
 import UIKit
 import CoreLocation
 
-class ViewController: UIViewController, UIGestureRecognizerDelegate {
+class ViewController: UIViewController, UIGestureRecognizerDelegate, UICollectionViewDelegate, UICollectionViewDataSource,
+UICollectionViewDelegateFlowLayout {
+    
     
     //References to items in the storyboard
-    @IBOutlet weak var scrollContainer: UIScrollView!
-    @IBOutlet weak var buttonCollector: UIStackView!
+    
+    @IBOutlet weak var buttonCollector: UICollectionView!
     @IBOutlet weak var refreshButton: UIButton!
     
     //Arrays that together make up an ordered pseudo-dictionary
     var toDos: [String] = []
     var states: [Bool] = []
     
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return toDos.endIndex
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = buttonCollector.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as! CollectionViewCell
+        
+        cell.displayContent(toDoName: toDos[indexPath.row], isDone: states[indexPath.row], instance: self)
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, canMoveItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: UIScreen.main.bounds.size.width, height: 50)
+    }
+    
     override func viewDidLoad() {
+        super.viewDidLoad()
         refreshButton.addTarget(self, action: #selector(updateButtonSet), for: .touchUpInside)
         updateButtonSet()
     }
@@ -41,85 +68,22 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
             guard data != nil else {return}
             let jsonObj = try? JSONSerialization.jsonObject(with: data!, options: []) as! NSDictionary
             self.toDos = jsonObj?.allKeys as! [String]
+            print(self.toDos)
             self.toDos.sort()
             self.states = [Bool](repeating: true, count: self.toDos.endIndex)
             for x in self.toDos.startIndex..<self.toDos.endIndex
             {
                 self.states[x] = jsonObj?.value(forKey: self.toDos[x]) as! Bool
             }
-            //DispatchQueue.main.async {
-            
             //Refreshes the views
-            self.redrawAllButtons()
-            print("Updated")
-            //}
+            DispatchQueue.main.async {
+                self.buttonCollector.reloadData()
+                //self.buttonCollector.setNeedsDisplay()
+                print("Updated")
+            }
         }
         //Starts the task
         task.resume()
-    }
-    
-    func redrawAllButtons()
-    {
-        print(self.toDos)
-        print(self.states)
-        DispatchQueue.main.async {
-            let buttons = self.buttonCollector.arrangedSubviews as! [UIButton]
-            //Loop through the data keys array, to show the data on screen
-            for x in self.toDos.startIndex..<self.toDos.endIndex
-            {
-                //If there is no next button, make one with the needed values
-                if (x >= buttons.endIndex)
-                {
-                    self.buildToDo(toDoSet: self.toDos[x], isDoneSet: self.states[x])
-                }
-                    //If there is a next button, make it match the keys and value
-                else
-                {
-                    print("Editing current button")
-                    let button = buttons[x]
-                    //DispatchQueue.main.async {
-                    button.setTitle(self.toDos[x], for: .normal)
-                    //}
-                    if (self.states[x])
-                    {
-                        //If the task is done, make the button green
-                        //DispatchQueue.main.async {
-                        print("Making a green button")
-                        button.layer.backgroundColor = UIColor.green.cgColor
-                        button.setNeedsDisplay()
-                        print("Made a green button")
-                        //}
-                        
-                    }
-                    else
-                    {
-                        //If the task is not done, make it be red
-                        //DispatchQueue.main.async {
-                        print("Making a red button")
-                        button.layer.backgroundColor = UIColor.red.cgColor
-                        button.setNeedsDisplay()
-                        //}
-                    }
-                }
-            }
-            //Trim off any extra buttons
-            if (self.toDos.endIndex <= buttons.endIndex)
-            {
-                print("Trimming buttons")
-                for x in self.toDos.endIndex..<buttons.endIndex
-                {
-                    //DispatchQueue.main.async {
-                    self.buttonCollector.removeArrangedSubview(buttons[x])
-                    self.buttonCollector.setNeedsDisplay()
-                    //}
-                }
-            }
-            self.buttonCollector.reloadInputViews()
-            self.buttonCollector.setNeedsDisplay()
-            print(self.toDos)
-            print(self.states)
-        }
-        print("Out")
     }
     
     func addNewToDo(toDoSet: String) {
@@ -137,8 +101,8 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
             request.addValue("application/json", forHTTPHeaderField: "Content-Type")
             request.addValue("application/json", forHTTPHeaderField: "Accept")
             
-            let task = URLSession.shared.dataTask(with: request){ (data, response, error) in
-                //Parse the data in the response and update the UI based on that
+            let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+                //Parses data into the toDos and states fields
                 guard data != nil else {return}
                 let jsonObj = try? JSONSerialization.jsonObject(with: data!, options: []) as! NSDictionary
                 self.toDos = jsonObj?.allKeys as! [String]
@@ -148,9 +112,12 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
                 {
                     self.states[x] = jsonObj?.value(forKey: self.toDos[x]) as! Bool
                 }
-                //DispatchQueue.main.async {
-                self.redrawAllButtons()
-                //}
+                //Refreshes the views
+                DispatchQueue.main.async {
+                    self.buttonCollector.reloadData()
+                    //self.buttonCollector.setNeedsDisplay()
+                    print("Adding")
+                }
             }
             task.resume()
         } catch {
@@ -168,21 +135,6 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
         
         states[index!] = !states[index!]
         
-        //Commented out section seems to have no effect
-        
-        /*DispatchQueue.main.async {
-         if (self.states[index!])
-         {
-         sender.layer.backgroundColor = UIColor.green.cgColor
-         sender.layer.setNeedsDisplay()
-         }
-         else
-         {
-         sender.layer.backgroundColor = UIColor.red.cgColor
-         sender.layer.setNeedsDisplay()
-         }
-         }*/
-        
         json[toDoName!] = states[index!]
         do {
             let data = try JSONSerialization.data(withJSONObject: json, options: [])
@@ -194,75 +146,27 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
             request.addValue("application/json", forHTTPHeaderField: "Accept")
             
             let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-                //Parse data and update UI based on response
+                //Parses data into the toDos and states fields
                 guard data != nil else {return}
                 let jsonObj = try? JSONSerialization.jsonObject(with: data!, options: []) as! NSDictionary
                 self.toDos = jsonObj?.allKeys as! [String]
                 self.toDos.sort()
+                self.states = [Bool](repeating: true, count: self.toDos.endIndex)
                 for x in self.toDos.startIndex..<self.toDos.endIndex
                 {
                     self.states[x] = jsonObj?.value(forKey: self.toDos[x]) as! Bool
                 }
-                //DispatchQueue.main.async {
-                self.redrawAllButtons()
-                //}
+                //Refreshes the views
+                DispatchQueue.main.async {
+                    self.buttonCollector.reloadData()
+                    //self.buttonCollector.setNeedsDisplay()
+                    print("Toggled")
+                }
             }
             task.resume()
         } catch {
             
         }
-    }
-    
-    func buildToDo(toDoSet: String, isDoneSet: Bool) {
-        //Add a new button to the stack view with the necessary title, color, actions, and constraints
-        DispatchQueue.main.async {
-            print("Making new button")
-            let toAdd = self.newButton(toDo: toDoSet, isDone: isDoneSet)
-            self.buttonCollector.addArrangedSubview(toAdd)
-            let xConstraint = NSLayoutConstraint(item: toAdd, attribute: NSLayoutAttribute.centerX, relatedBy: NSLayoutRelation.equal, toItem: self.buttonCollector, attribute: NSLayoutAttribute.centerX, multiplier: 1, constant: 0)
-            self.buttonCollector.addConstraint(xConstraint)
-            let widthConstraint = NSLayoutConstraint(item: toAdd, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: UIScreen.main.bounds.size.width)
-            self.buttonCollector.addConstraint(widthConstraint)
-            let heightConstraint = NSLayoutConstraint(item: toAdd, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: 50)
-            toAdd.addConstraint(heightConstraint)
-        }
-    }
-    
-    func newButton(toDo: String, isDone: Bool) -> UIButton
-    {
-        //Make a new button to return
-        let toReturn = UIButton(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
-        
-        //Set button action
-        toReturn.addTarget(self, action: #selector(toggleButton), for: .touchUpInside)
-        
-        //Create and add a swipe gesture recognizer
-        let swiper = UISwipeGestureRecognizer(target: self, action: #selector(self.doSwipe))
-        swiper.delegate = self
-        swiper.direction = .left
-        toReturn.addGestureRecognizer(swiper)
-        
-        //Set the button title and color
-        toReturn.setTitle(toDo, for: .normal)
-        if (isDone)
-        {
-            toReturn.layer.backgroundColor = UIColor.green.cgColor
-        }
-        else {
-            toReturn.layer.backgroundColor = UIColor.red.cgColor
-        }
-        
-        //Make the button look good
-        toReturn.layer.cornerRadius = 10
-        toReturn.layer.borderWidth = 2
-        toReturn.layer.borderColor = UIColor.white.cgColor
-        toReturn.layer.shadowColor = UIColor.black.cgColor
-        toReturn.layer.shadowOpacity = 0.6
-        toReturn.layer.shadowRadius = 3
-        toReturn.layer.shadowOffset = CGSize(width: 2, height: 2)
-        
-        //Return
-        return toReturn
     }
     
     @objc func doSwipe(sender: UIGestureRecognizer!)
@@ -286,28 +190,28 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
                 request.addValue("application/json", forHTTPHeaderField: "Content-Type")
                 request.addValue("application/json", forHTTPHeaderField: "Accept")
                 
-                let task = URLSession.shared.dataTask(with: request) {(data, response, error) in
-                    //Parse response data and redraw buttons based on result
+                let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+                    //Parses data into the toDos and states fields
                     guard data != nil else {return}
-                    print(String(data: data!, encoding: .utf8)!)
                     let jsonObj = try? JSONSerialization.jsonObject(with: data!, options: []) as! NSDictionary
                     self.toDos = jsonObj?.allKeys as! [String]
                     self.toDos.sort()
+                    self.states = [Bool](repeating: true, count: self.toDos.endIndex)
                     for x in self.toDos.startIndex..<self.toDos.endIndex
                     {
                         self.states[x] = jsonObj?.value(forKey: self.toDos[x]) as! Bool
                     }
-                    //DispatchQueue.main.async {
-                    self.redrawAllButtons()
-                    //}
+                    //Refreshes the views
+                    DispatchQueue.main.async {
+                        self.buttonCollector.reloadData()
+                        //self.buttonCollector.setNeedsDisplay()
+                        print("Deleted")
+                    }
                 }
                 
                 task.resume()
             } catch {
                 
-            }
-            DispatchQueue.main.async() {
-                self.buttonCollector.removeArrangedSubview(sender.view!)
             }
         }
         else
